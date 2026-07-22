bd37ebdf6d73b067a04fbf34766c5a7ede2ad8c3
Nanadgopan <nandhukr006@gmail.com>
2026-07-22 09:07:19 +0530

    feat: implement payment history API

--- backend/apps/payments/views.py
+++ backend/apps/payments/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import  PaymentsSerializer
+from .models import Payment
 
 class PaymentsAPIView(APIView):
     permission_classes=[IsAuthenticated]
@@ -18,4 +19,14 @@ class PaymentsAPIView(APIView):
                 },status=201
             )
         return Response( serializer.errors,status=401)
+    
+    def get(self,request):
+        
+        payments=Payment.objects.filter(user=request.user)
+        
+        serializer=PaymentsSerializer(payments,many=True)
+        
+        return Response(serializer.data,status=200)
+    
+    
     
